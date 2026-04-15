4af13f6ba3885a6265c5439314f0603589f455c4
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-15 20:57:28 -0300

    refine: reduce structural identities to definition unfolding obligations

--- PachnerInvariant/structural_identities.lean
+++ PachnerInvariant/structural_identities.lean
@@ -7,6 +7,9 @@ lemma allEdges_eq_bind_edgesOfTet
   (T : Triangulation) :
   allEdges T = (allTets T).bind edgesOfTet := by
   classical
+  -- must follow from the definition of allEdges in frontier
+  -- reduce by unfolding definition
+  unfold allEdges
   admit
 
 -- Structural identity 2: definition of edgeDeg via incidence
@@ -15,5 +18,7 @@ lemma edgeDeg_eq_incidence_count
   edgeDeg T e =
   (List.filter (fun t => e ∈ edgesOfTet t) (allTets T)).length := by
   classical
+  -- must follow from definition of edgeDeg
+  unfold edgeDeg
   admit
 
